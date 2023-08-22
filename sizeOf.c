d0bd1c13390ed7b1542a611a10d1530ea7242408
nitinbhopale <nitinbhopale53@gmail.com>
2023-08-22 20:32:10 +0530

    cmt

--- sizeOf.c
+++ sizeOf.c
@@ -7,10 +7,10 @@ int main()
     float f = 12.31f;
     double d = 89.236589;
 
-    printf("Size of Character: %d\n",sizeof(ch));
-    printf("Size of integer: %d\n",sizeof(i));
-    printf("Size of float: %d\n",sizeof(f));
-    printf("Size of double: %d\n",sizeof(d));
-    
+    printf("Size of Character: %d bytes\n",sizeof(ch));
+    printf("Size of integer: %d bytes\n",sizeof(i));
+    printf("Size of float: %d bytes\n",sizeof(f));
+    printf("Size of double: %d bytes\n",sizeof(d));
+
     return 0;
 }

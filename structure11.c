e86b1e9e3f71b2ddef942d70fadb8e91c4e6d8a7
nitinbhopale <nitinbhopale53@gmail.com>
2023-09-07 19:52:23 +0530

    strcut

--- structure11.c
+++ structure11.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 
+#pragma pack(1)
+
+struct Demo
+{
+    int i;
+    char ch;
+    float f;
+    double d;
+    
+};
 int main()
 {
-    
+    struct Demo obj;
+
+    printf("Size of object is :%d\n",sizeof(obj));
+
     return 0;
 }

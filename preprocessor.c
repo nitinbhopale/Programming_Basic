ecd35aba3d4d61928a3cab10a3d8f1879111d2f7
nitinbhopale <nitinbhopale53@gmail.com>
2023-09-08 20:35:03 +0530

    pre

---
+++ preprocessor.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include "marvellous.h"
+
+#define PI 3.14
+#define Dozen 12
+
+int main()
+{
+    struct Demo obj;
+
+    float Value = 10 * 10 * PI;
+
+    int Count =  10 * Dozen;
+
+    printf("Value of No: %d\n",No);
+    printf("Area is :%.1f\n",Value);
+    printf("Count is :%d\n",Count);
+    return 0;
+}

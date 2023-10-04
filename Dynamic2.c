4a6d90ab10882da24f999cad5ad8b35273ae79e4
nitinbhopale <nitinbhopale53@gmail.com>
2023-10-04 20:45:09 +0530

    dy

--- Dynamic2.c
+++ Dynamic2.c
@@ -4,17 +4,14 @@
 
 int main()
 {
-    int Arr[5];     // Static Memory
-    float fValue;   // Static Memory
-    double Brr[4];  // Static Memory
-
+   
     int iSize = 0;
     int *ptr = NULL;
 
     printf("Enter the size of array : \n");
     scanf("%d",&iSize);
 
-    ptr = (int *)malloc(iSize * sizeof(int));
+    ptr = (int *)calloc(iSize , sizeof(int));
 
     ptr[0] = 10;
     ptr[1] = 11;
@@ -22,5 +19,7 @@ int main()
    
     free(ptr);
 
+
+    
     return 0;
 }
